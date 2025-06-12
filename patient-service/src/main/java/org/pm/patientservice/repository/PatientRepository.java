31062a6756ee3f1dee83da504dcac21dc132270c
omer358 <omermaki358@gmail.com>
2025-06-12 07:17:44 +0300

    configure h2 db, patient entity and repo

---
+++ patient-service/src/main/java/org/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package org.pm.patientservice.repository;
+
+import org.pm.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
