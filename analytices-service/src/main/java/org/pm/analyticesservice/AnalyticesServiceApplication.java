5277eaa64721f28b0af4f899e81f6590dddb1eaf
omer358 <omermaki358@gmail.com>
2025-06-17 01:20:30 +0300

    initialize API Gateway service with Spring Boot, configure routes to patient service, update patient service HTTP requests, and add Maven, Docker, and IntelliJ setup

---
+++ analytices-service/src/main/java/org/pm/analyticesservice/AnalyticesServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pm.analyticesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticesServiceApplication.class, args);
+	}
+
+}
