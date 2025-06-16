161ae027671c0b837097d6abb26fbe1ecbbbe64a
omer358 <omermaki358@gmail.com>
2025-06-17 01:20:30 +0300

    initialize analytices-service with Spring Boot, add Kafka consumer for patient events, configure Maven and Docker, and define proto file for patient events

---
+++ analytices-service/src/test/java/org/pm/analyticesservice/AnalyticesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pm.analyticesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
